0230e376ca98442d52fa3d6d208c042cb6250541
kureha8827 <kurehasub719@gmail.com>
2024-03-15 10:45:34 +0900

    Initial Commit

---
+++ luminous/luminousApp.swift
@@ -0,0 +1,17 @@
+//
+//  luminousApp.swift
+//  luminous
+//
+//  Created by kureha8827 on 2024/03/15.
+//
+
+import SwiftUI
+
+@main
+struct luminousApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
